ee930c43bb231d9f5d7ba4cd0ea3db48fc44a3b4
vieerr <olivier_paspuel@hotmail.com>
2025-09-15 23:56:28 -0500

    ci(general): 🎡 env check for mf remote

--- shell/vite.config.ts
+++ shell/vite.config.ts
@@ -2,24 +2,29 @@ import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import federation from "@originjs/vite-plugin-federation";
 import tailwindcss from "@tailwindcss/vite"
-export default defineConfig({
-  plugins: [
-    vue(),
-    tailwindcss(),
-    federation({
-      name: "shell",
-      filename: "remoteEntry.js",
-      remotes: {
-        invoices_mf: "http://localhost:4174/assets/remoteEntry.js", // Adjust port
-        // invoices_mf: "http://localhost:4175/assets/remoteEntry.js", // Adjust port
-      },
-      shared:["vue", "@tanstack/vue-query", "vue-router"]
-    }),
-  ],
-  server: {
-    port: 5173, // Shell port
-  },
-  build: {
-    target: "esnext", // Required for Module Federation
-  },
+export default defineConfig(({ mode }) => {
+  const isProduction = mode === "production";
+
+  return {
+    plugins: [
+      vue(),
+      tailwindcss(),
+      federation({
+        name: "shell",
+        filename: "remoteEntry.js",
+        remotes: {
+          invoices_mf: isProduction
+            ? "https://trista-invoices.vercel.app/assets/remoteEntry.js"
+            : "http://localhost:4174/assets/remoteEntry.js", // Adjust port
+        },
+        shared: ["vue", "@tanstack/vue-query", "vue-router"],
+      }),
+    ],
+    server: {
+      port: 5173, // Shell port
+    },
+    build: {
+      target: "esnext", // Required for Module Federation
+    },
+  };
 });
