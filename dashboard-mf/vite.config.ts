bd7acb93228675853023a9c1e23681083848d1af
vieerr <olivier_paspuel@hotmail.com>
2025-09-19 19:15:26 -0500

    feat(dashboard-mf): 🎸 add shadcn configuration

--- dashboard-mf/vite.config.ts
+++ dashboard-mf/vite.config.ts
@@ -1,18 +1,13 @@
-import { fileURLToPath, URL } from 'node:url'
-
-import { defineConfig } from 'vite'
+import path from 'node:path'
+import tailwindcss from '@tailwindcss/vite'
 import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
+import { defineConfig } from 'vite'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
+  plugins: [vue(), tailwindcss()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': path.resolve(__dirname, './src'),
     },
   },
 })
